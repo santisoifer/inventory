3749a6ea5a6b50ea791516fa35914845d4973a32
Santi Soifer <santisoi2008@gmail.com>
2022-10-18 13:12:43 -0300

    Added the inventary page (home.ejs)

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.get("/", function(req, res){
     res.render("signup.ejs")
 });
 
+app.get("/home", function(req, res){
+    res.render("home.ejs")
+});
+
 app.listen(3000, function(){
     console.log("Server listening on port 3000");
 });
